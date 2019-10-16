c34dfba6614110f0e9b2c742234bc790a0df6eae
ApexWilson <wongwilson112@gmail.com>
2019-10-16 01:28:35 +0000

    fix logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $( "document" ).ready(function() {
 
+ var inputValue=$('.input').val();
 
+
+function checkVowel(string){
+    var firstLetter= string[0];
+    var vowels=['a','e','i','o','u'];
+    if (vowels.includes(firstLetter)){
+        $('.output').text('This is a vowel!');
+    }
+        $('.output').text('This is a consonant!');
+    }
+
+$('#button').click(function() {
+    checkVowel(inputValue);
+})
 
 });
